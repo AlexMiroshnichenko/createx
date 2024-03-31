3592c2a140303953828815cf9addeed60627e9f4
Alex Miroshnichenko <miroshnichenko.ua@gmail.com>
2024-03-31 20:19:59 +0300

    Create section partfolio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+//Инициализация слайдера Swiper (секция партфолио)
+const portfolioSlider = new Swiper('.portfolio-slider', {
+  // Парметры настройки
+  slidesPerView: 3,
+  spaceBetween: 30,
+  speed: 500,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+
+  // Навигационные стрелки
+  navigation: {
+    nextEl: '.slider-control-next',
+    prevEl: '.slider-control-prev',
+  },
+});
--- js/swiper.js
+++
@@ -1,8 +0,0 @@
-const reviewsSwiper = new Swiper('.reviews-swiper', {
-  loop: true,
-  navigation: {
-    nextEl: '.reviews-btn-prev',
-    prevEl: '.reviews-btn-next',
-  },
-  speed: 1000,
-});
